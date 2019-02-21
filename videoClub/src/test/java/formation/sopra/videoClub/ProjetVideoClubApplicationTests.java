928e240822aaf6cec972527e5663eb18e3a9a650
gauthierpalma <47034019+gauthierpalma@users.noreply.github.com>
2019-02-21 12:10:32 +0100

    Add files via upload

---
+++ videoClub/src/test/java/formation/sopra/videoClub/ProjetVideoClubApplicationTests.java
@@ -0,0 +1,16 @@
+package formation.sopra.videoClub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetVideoClubApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
